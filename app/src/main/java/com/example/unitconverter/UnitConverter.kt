dc9ab4a95150a9412b8163139d41f326d31212b7
Imran Ahmad <imranjmi463@gmail.com>
2024-06-08 17:48:33 +0530

    logic correction

---
+++ app/src/main/java/com/example/unitconverter/UnitConverter.kt
@@ -0,0 +1,151 @@
+package com.example.unitconverter
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import kotlin.math.roundToInt
+
+@Composable
+fun UnitConverter() {
+
+    var inputValue by remember { mutableStateOf("") }
+    var outputValue by remember { mutableStateOf("") }
+    var inputUnit by remember { mutableStateOf("Meters") }
+    var outputUnit by remember { mutableStateOf("Meters") }
+    var iExpanded by remember { mutableStateOf(false) }
+    var oExpanded by remember { mutableStateOf(false) }
+    val conversionFactor = remember { mutableStateOf(1.0) }
+    val oconversionFactor = remember { mutableStateOf(1.0) }
+
+    fun convertUnit() {
+        val inputValueDouble = inputValue.toDoubleOrNull() ?: 0.0
+        val resultInputInMeter = inputValueDouble * conversionFactor.value
+        val resultOutput =
+            ((resultInputInMeter * oconversionFactor.value * 100.0).roundToInt()) / 100.0
+        outputValue = resultOutput.toString()
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Unit Converter", style = MaterialTheme.typography.headlineMedium)
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            value = inputValue,
+            onValueChange = {
+                inputValue = it
+                convertUnit()
+            },
+            label = { Text(text = "Enter Value") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Row {
+            //input box
+            Box {
+                //input button
+                Button(onClick = { iExpanded = true }) {
+                    Text(text = inputUnit)
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow down")
+                }
+                DropdownMenu(expanded = iExpanded, onDismissRequest = { iExpanded = false }) {
+                    //here text not expecting a string it is expecting a composable that's why we passed a composable text inside that we passed a string
+                    DropdownMenuItem(text = { Text("Centimeters") }, onClick = {
+                        iExpanded = false
+                        inputUnit = "Centimeters"
+                        conversionFactor.value = 0.01
+                        convertUnit()
+                    })
+                    DropdownMenuItem(text = { Text(text = "Meters") }, onClick = {
+                        iExpanded = false
+                        inputUnit = "Meters"
+                        conversionFactor.value = 1.0
+                        convertUnit()
+                    })
+                    DropdownMenuItem(text = { Text("Millimeter") }, onClick = {
+                        iExpanded = false
+                        inputUnit = "Millimeter"
+                        conversionFactor.value = 0.001
+                        convertUnit()
+                    })
+                    DropdownMenuItem(text = { Text("Feet") }, onClick = {
+                        iExpanded = false
+                        inputUnit = "Feet"
+                        conversionFactor.value = 0.3048
+                        convertUnit()
+                    })
+                }
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            //output box
+            Box {
+                // output button
+                Button(onClick = { oExpanded = true }) {
+                    Text(text = outputUnit)
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow down")
+                }
+                DropdownMenu(expanded = oExpanded, onDismissRequest = { oExpanded = false }) {
+                    //here text not expecting a string it is expecting a composable that's why we passed a composable text inside that we passed a string
+                    DropdownMenuItem(text = { Text("Centimeters") }, onClick = {
+                        oExpanded = false
+                        outputUnit = "Centimeters"
+                        oconversionFactor.value = 100.0
+                        convertUnit()
+                    })
+                    DropdownMenuItem(text = { Text(text = "Meters") }, onClick = {
+                        oExpanded = false
+                        outputUnit = "Meters"
+                        oconversionFactor.value = 1.00
+                        convertUnit()
+                    })
+                    DropdownMenuItem(text = { Text("Millimeter") }, onClick = {
+                        oExpanded = false
+                        outputUnit = "Millimeter"
+                        oconversionFactor.value = 1000.0
+                        convertUnit()
+                    })
+                    DropdownMenuItem(text = { Text("Feet") }, onClick = {
+                        oExpanded = false
+                        outputUnit = "Feet"
+                        oconversionFactor.value = 3.28
+                        convertUnit()
+                    })
+
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "Result: $outputValue $outputUnit",
+            style = MaterialTheme.typography.headlineSmall
+        )
+    }
+}
